b246b8f4ca9a076c63d296a38291f208981cbbe2
unknown <masterrio2412@gmail.com>
2025-03-31 10:21:31 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename="/ShopIphoneByReactJs">
+    <BrowserRouter basename="/ShopIphoneByReactJs/">
       <App />
     </BrowserRouter>
   </Provider>
